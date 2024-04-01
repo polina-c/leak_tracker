0b14eeeebb7070de8cb4bf924871644af8324edc
Polina Cherkasova <polina.c@live.com>
2024-04-01 10:34:35 -0700

    Update _finalizer.dart

--- pkgs/memory_usage/lib/src/weak_cache/_finalizer.dart
+++ pkgs/memory_usage/lib/src/weak_cache/_finalizer.dart
@@ -5,7 +5,29 @@
 typedef ObjectGcCallback = void Function(Object token);
 
 /// Finalizer builder to mock standard [Finalizer].
-typedef FinalizerBuilder = Finalizer Function(ObjectGcCallback onObjectGc);
+typedef FinalizerBuilder = FinalizerWrapper Function(
+  ObjectGcCallback onObjectGc,
+);
 
-Finalizer buildFinalizer(ObjectGcCallback onObjectGc) =>
-    Finalizer<Object>(onObjectGc);
+FinalizerWrapper buildStandardFinalizer(ObjectGcCallback onObjectGc) =>
+    StandardFinalizerWrapper(onObjectGc);
+
+abstract class FinalizerWrapper {
+  void attach(Object object, Object token);
+}
+
+/// Finalizer wrapper to mock standard [Finalizer].
+///
+/// Needed because the standard [Finalizer] is final and
+/// thus cannot be implemented.
+class StandardFinalizerWrapper implements FinalizerWrapper {
+  StandardFinalizerWrapper(ObjectGcCallback onObjectGc)
+      : _finalizer = Finalizer<Object>(onObjectGc);
+
+  final Finalizer<Object> _finalizer;
+
+  @override
+  void attach(Object object, Object token) {
+    _finalizer.attach(object, token);
+  }
+}
